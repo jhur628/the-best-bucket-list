2a97d0ac19d9f105c15277144d961355e05f6aad
John Hur <hur.john628@gmail.com>
2022-02-22 21:47:43 -0600

    worked on some components

--- src/components/bucketForm.js
+++ src/components/bucketForm.js
@@ -1,11 +1,45 @@
 import React, { useState } from 'react';
 
-export default function BucketForm() {
+export default function BucketForm(props) {
+    // Create state for text input field and function to set state
+    const [input, setInput] = useState('');
+    // Create eagerness state and function to set state
+    let [eagerness, setEagerness] = useState('');
+
+    // Create an eagerness level array
+    const eagernessLevel = ['high', 'medium', 'low'];
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Invokes addButton function that has been passed through to bucketForm components
+        // Sets in an object id, text, and level of eagerness into bucket array
+        props.onSubmit({
+            id: Math.random(Math.floor() * 1000),
+            text: input,
+            eagerness: eagerness
+        });
+
+        // Resets input state and eagerness state
+        setInput('');
+        setEagerness('');
+    };
+
+    const handleChange = (event) => {
+        // When the text input is changed the input is set to whatever was changed
+        setInput(event.target.value);
+    };
 
     return (
         <div>
             <form>
-                <input type="text" placeholder="Your bucket here..." />
+                <input 
+                    type="text" 
+                    placeholder="Your bucket here..."
+                    value={input}
+                    className=""
+                    onChange={handleChange}
+                />
                 <button>Add bucket!</button>
             </form>
         </div>
--- src/components/bucketList.js
+++ src/components/bucketList.js
@@ -3,10 +3,28 @@ import Bucket from './bucket';
 import BucketForm from './bucketForm';
 
 export default function BucketList() {
+    // Create bucket state and function to set bucket state
+    const [bucket, setBucket] = useState([]);
+
+    const addBucket = (item) => {
+        console.log(`Added a bucket: `, item)
+
+        // If there is no text return the function
+        if (!item.text) {
+            return;
+        };
+
+        // Add the new item into the bucket array of objects
+        const newBucket = [item, ...bucket];
+        console.log(newBucket);
+
+        // set bucket using newBucket
+        setBucket(newBucket);
+    }
 
     return (
         <div>
-            <BucketForm />
+            <BucketForm onSubmit={addBucket} />
             <Bucket />
         </div>
     )
